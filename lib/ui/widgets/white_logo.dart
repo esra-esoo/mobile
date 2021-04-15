5ec11e2d31fb7f94639720dc7883bbb8b9701c85
Youssef Gheith <you.gheith@gmail.com>
2021-04-15 23:01:45 +0200

    logo

--- lib/ui/widgets/white_logo.dart
+++ lib/ui/widgets/white_logo.dart
@@ -9,7 +9,7 @@ class WhiteLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Image.asset(
-      'assets/images/white_logo.png',
+      'assets/images/logo_white.png',
       fit: BoxFit.contain,
       height: height,
       width: width ?? double.infinity,
