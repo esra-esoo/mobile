8df086031fa6ec3bf1e46cd1123363929b7fb9d9
Youssef Gheith <you.gheith@gmail.com>
2021-04-30 15:14:46 +0200

    add file extension methods

---
+++ lib/extensions/file_extensions.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+extension FileExtention on FileSystemEntity {
+  get getName {
+    return this?.path?.split(Platform.pathSeparator)?.last;
+  }
+
+  get getExtension {
+    return this?.path?.split(".")?.last?.trim();
+  }
+}
