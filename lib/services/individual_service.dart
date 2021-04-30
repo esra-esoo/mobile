ecd7deabebfff29487edc436dc6883ea2d5b3c56
Youssef Gheith <you.gheith@gmail.com>
2021-04-30 15:15:29 +0200

    add create file

--- lib/services/individual_service.dart
+++ lib/services/individual_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:huayati/app/locator.dart';
+import 'package:huayati/models/indivisual_create_file_payload.dart';
 
 import 'api.dart';
 
@@ -16,6 +17,22 @@ class IndividualService {
     } catch (e) {
       print(e);
       return false;
+    }
+  }
+
+  Future<void> createFiles(IndivisualCreateFilePayload payload) async {
+    try {
+      print(payload.toJson());
+      return;
+      await _api.putCallWithToken(
+        url: '/api/Individual/CreateFiles',
+        data: payload,
+      );
+      return true;
+    } on DioError catch (e) {
+      throw e;
+    } catch (e) {
+      throw e;
     }
   }
 }
