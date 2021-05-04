cbc56fa7d3503218a49be063ca51eeda82c293eb
Youssef Gheith <you.gheith@gmail.com>
2021-05-04 22:37:46 +0200

    create company

---
+++ lib/ui/views/company/update_files/update_files_view.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:huayati/consts/styles.dart';
+import 'package:huayati/ui/widgets/busy_overlay.dart';
+import 'package:huayati/ui/widgets/form/bottom_submit_button.dart';
+import 'package:huayati/ui/widgets/form/form_title.dart';
+import 'package:huayati/ui/widgets/form/image_update_field.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:stacked/stacked.dart';
+
+import 'update_files_viewmodel.dart';
+
+class CompanyUpdateFilesView extends StatelessWidget {
+  const CompanyUpdateFilesView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<CompanyUpdateFilesViewModel>.reactive(
+      viewModelBuilder: () => CompanyUpdateFilesViewModel(),
+      onModelReady: (viewModel) => viewModel.initilizeView(),
+      builder: (context, viewModel, child) => BusyOverlay(
+        primaryColors: true,
+        show: viewModel.isBusy,
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            leading: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: Icon(Icons.arrow_back_ios, color: kcolorPrimaryBlue),
+            ),
+            centerTitle: true,
+            title: FormTitle(
+              title: 'مستندات الشركة',
+              color: kcolorPrimaryBlue,
+            ),
+          ),
+          body: Padding(
+            padding: EdgeInsets.only(
+              top: 30.h,
+              left: 30.w,
+              right: 30.w,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const _RefuseStateMessage(),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: viewModel.imageFiles.length,
+                    itemBuilder: (context, index) {
+                      return ImageUpdateField(
+                        fileName: viewModel.imageFiles[index].name,
+                        base64Content:
+                            viewModel.imageFiles[index].base64Content,
+                        isEditDisabled:
+                            viewModel.imageFiles[index].isEditDisabled,
+                        newFile: viewModel.getNewRawImageFileById(
+                          viewModel.imageFiles[index].companyFileId,
+                        ),
+                        onChanged: (file) => viewModel.addToNewImageFiles(
+                          viewModel.imageFiles[index],
+                          file,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          bottomNavigationBar: BottomSubmitButton(
+            label: 'حفظ التغييرات ',
+            accentColors: false,
+            onPressed: () => viewModel.saveData(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RefuseStateMessage extends ViewModelWidget<CompanyUpdateFilesViewModel> {
+  const _RefuseStateMessage();
+
+  @override
+  Widget build(
+    BuildContext context,
+    CompanyUpdateFilesViewModel viewModel,
+  ) {
+    return viewModel.refuseMessage != null
+        ? Padding(
+            padding: EdgeInsets.only(bottom: 20.h),
+            child: Text(
+              viewModel.refuseMessage,
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: getValueForScreenType<double>(
+                  context: context,
+                  mobile: 16,
+                  tablet: 18,
+                ),
+              ),
+            ),
+          )
+        : SizedBox.shrink();
+  }
+}
---
+++ lib/ui/views/company/update_files/update_files_viewmodel.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:huayati/models/company/image_file.dart';
+import 'package:huayati/models/company/image_raw_file.dart';
+import 'package:huayati/models/navigation_result.dart';
+import 'package:huayati/services/company_service.dart';
+import 'package:huayati/services/shared_service.dart';
+import 'package:huayati/services/third_party/dialog_service.dart';
+import 'package:huayati/services/third_party/navigation_service.dart';
+import 'package:huayati/ui/widgets/success_update_modal.dart';
+import 'package:huayati/utils/file_utils.dart';
+import 'package:stacked/stacked.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:huayati/app/locator.dart';
+import 'package:huayati/services/third_party/snackbar_service.dart';
+import 'package:huayati/extensions/file_extensions.dart';
+
+class CompanyUpdateFilesViewModel extends BaseViewModel {
+  final _companyService = locator<CompanyService>();
+  final _snackbarService = locator<SnackbarService>();
+  final _navigationService = locator<NavigationService>();
+  final _dialogService = locator<DialogService>();
+  final _sharedService = locator<SharedService>();
+
+  List<CompanyImageFile> imageFiles = [];
+  List<CompanyImageRawFile> newImageFiles = [];
+  String refuseMessage;
+
+  File getNewRawImageFileById(String companyFileId) {
+    var index = newImageFiles.indexWhere(
+      (element) => element.companyFileId == companyFileId,
+    );
+    if (index == -1)
+      return null;
+    else
+      return newImageFiles[index].file;
+  }
+
+  Future addToNewImageFiles(CompanyImageFile imageFile, File file) async {
+    var index = newImageFiles.indexWhere(
+      (element) => element.companyFileId == imageFile.companyFileId,
+    );
+    if (index == -1) {
+      newImageFiles.add(
+        CompanyImageRawFile(
+          companyFileId: imageFile.companyFileId,
+          name: imageFile.name,
+          file: file,
+          isEditDisabled: imageFile.isEditDisabled,
+          fileFullName: file.getName,
+          editBtnShow: imageFile.editBtnShow,
+        ),
+      );
+    } else {
+      newImageFiles[index] = CompanyImageRawFile(
+        companyFileId: imageFile.companyFileId,
+        name: imageFile.name,
+        file: file,
+        isEditDisabled: imageFile.isEditDisabled,
+        fileFullName: file.getName,
+        editBtnShow: imageFile.editBtnShow,
+      );
+    }
+    notifyListeners();
+  }
+
+  void initilizeView() async {
+    try {
+      refuseMessage =
+          _sharedService?.sharedRefuseState?.companyRefuseState?.refuseType;
+      imageFiles = await runBusyFuture(
+        _companyService.getCompanyImages(),
+        throwException: true,
+      );
+    } catch (e) {
+      await Future.delayed(const Duration(milliseconds: 500));
+      _navigationService.back(
+        result: NavigationResult(
+          success: false,
+          message: e.toString(),
+        ),
+      );
+    }
+  }
+
+  Future saveData() async {
+    if (newImageFiles.length != imageFiles.length) {
+      _snackbarService.showBottomErrorSnackbar(
+        message: 'يجب عليك إعادة رفع كافة المستندات',
+      );
+      return;
+    }
+    var response = await _dialogService.showConfirmDialog(
+      title: 'تأكيد العملية',
+      description: 'هل أنت متأكد من رغبتك في حفظ التغييرات ؟',
+    );
+    if (!response.confirmed) return;
+    await _uploadFiles();
+  }
+
+  Future _uploadFiles() async {
+    try {
+      setBusy(true);
+      var updatedImages = await _getUpdatedImageFiles();
+      await _companyService.changeCompanyImages(updatedImages);
+      await _sharedService.getRefuseState();
+      setBusy(false);
+      await _showSuccessModal();
+    } catch (e) {
+      setBusy(false);
+      print(e);
+      _snackbarService.showBottomErrorSnackbar(
+        message: e.toString(),
+      );
+    }
+  }
+
+  Future<List<CompanyImageFile>> _getUpdatedImageFiles() async {
+    List<CompanyImageFile> list = [];
+    for (var updatedImages in newImageFiles) {
+      var base64Content = await FileUtils.fromRawFileToBase64String(
+        updatedImages.file,
+      );
+      list.add(CompanyImageFile(
+        companyFileId: updatedImages.companyFileId,
+        name: updatedImages.name,
+        base64Content: base64Content,
+        editBtnShow: updatedImages.editBtnShow,
+        isEditDisabled: updatedImages.isEditDisabled,
+        fileFullName: updatedImages.fileFullName,
+      ));
+    }
+    return list;
+  }
+
+  Future _showSuccessModal() async {
+    await showGeneralDialog(
+      context: Get.overlayContext,
+      barrierColor: Colors.white,
+      barrierDismissible: false,
+      transitionDuration: const Duration(milliseconds: 400),
+      pageBuilder: (_, __, ___) => SuccessUpdateModal(),
+    );
+  }
+}
