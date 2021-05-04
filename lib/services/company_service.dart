0a42c204972810429262e8357493dec862c744c0
Youssef Gheith <you.gheith@gmail.com>
2021-05-04 15:45:32 +0200

    add service

--- lib/services/company_service.dart
+++ lib/services/company_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:huayati/app/locator.dart';
+import 'package:huayati/models/company/bank_account.dart';
 import 'package:huayati/models/company/company_create_files_payload.dart';
 import 'package:huayati/models/company/company_data.dart';
 import 'package:huayati/models/image_file.dart';
@@ -45,6 +46,23 @@ class CompanyService {
         url: '/api/Company/GetCompanyData',
       );
       return CompanyData.fromJson(response);
+    } on DioError catch (e) {
+      throw e.message;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<List<CompanyBankAccount>> getBankAccounts() async {
+    try {
+      final response = await _api.getCallWithToken(
+        url: '/api/Company/GetBankAccounts',
+      );
+      return response
+          ?.map<CompanyBankAccount>(
+            (json) => CompanyBankAccount.fromJson(json),
+          )
+          ?.toList();
     } on DioError catch (e) {
       throw e.message;
     } catch (e) {
