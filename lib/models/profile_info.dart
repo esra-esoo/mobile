45b2bbce4d023d34ab2e4e2ca1c74e2800bc8493
Youssef Gheith <you.gheith@gmail.com>
2021-05-05 14:38:38 +0200

    add pofile model

---
+++ lib/models/profile_info.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'profile_info.g.dart';
+
+@JsonSerializable(explicitToJson: false)
+class ProfileInfo {
+  final String username;
+  final String fullname;
+  final String familyName;
+  final String email;
+  final String phoneNumber;
+
+  const ProfileInfo({
+    this.username,
+    this.fullname,
+    this.familyName,
+    this.email,
+    this.phoneNumber,
+  });
+
+  factory ProfileInfo.fromJson(Map<String, dynamic> json) =>
+      _$ProfileInfoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ProfileInfoToJson(this);
+}
---
+++ lib/models/profile_info.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ProfileInfo _$ProfileInfoFromJson(Map<String, dynamic> json) {
+  return ProfileInfo(
+    username: json['username'] as String,
+    fullname: json['fullname'] as String,
+    familyName: json['familyName'] as String,
+    email: json['email'] as String,
+    phoneNumber: json['phoneNumber'] as String,
+  );
+}
+
+Map<String, dynamic> _$ProfileInfoToJson(ProfileInfo instance) =>
+    <String, dynamic>{
+      'username': instance.username,
+      'fullname': instance.fullname,
+      'familyName': instance.familyName,
+      'email': instance.email,
+      'phoneNumber': instance.phoneNumber,
+    };
