8c12f8c15530a5e0c771160b317ff944d6ba852f
Youssef Gheith <you.gheith@gmail.com>
2021-05-05 15:43:25 +0200

    organize

--- lib/ui/views/profile/profile_view.dart
+++ lib/ui/views/profile/profile_view.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:huayati/app/locator.dart';
+import 'package:huayati/consts/styles.dart';
+import 'package:huayati/ui/widgets/botton_padding.dart';
+import 'package:huayati/ui/widgets/data_item.dart';
+import 'package:huayati/ui/widgets/loading_indicator.dart';
+import 'package:huayati/ui/widgets/scrollbar.dart';
+import 'package:stacked/stacked.dart';
+
+import 'profile_viewmodel.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({Key key}) : super(key: key);
@@ -7,6 +17,105 @@ class ProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height,
+      child: ViewModelBuilder<ProfileViewModel>.reactive(
+        disposeViewModel: false,
+        fireOnModelReadyOnce: true,
+        initialiseSpecialViewModelsOnce: true,
+        viewModelBuilder: () => locator<ProfileViewModel>(),
+        onModelReady: (viewModel) => viewModel.initilizeView(),
+        builder: (context, viewModel, child) => AnimatedSwitcher(
+          duration: const Duration(milliseconds: 375),
+          child: viewModel.isBusy
+              ? const CenterLoadingIndicator()
+              : viewModel.profileInfo == null
+                  ? const _RefreshView()
+                  : const _DataView(),
+        ),
+      ),
+    );
+  }
+}
+
+class _DataView extends ViewModelWidget<ProfileViewModel> {
+  const _DataView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ProfileViewModel viewModel) {
+    return PlatformScrollBar(
+      child: Container(
+        constraints: BoxConstraints.expand(),
+        padding: EdgeInsets.only(
+          top: 30.h,
+          left: 30.w,
+          right: 30.w,
+        ),
+        child: RefreshIndicator(
+          backgroundColor: kcolorPrimaryBlue,
+          color: Colors.white,
+          onRefresh: () => viewModel.initilizeView(),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              SizedBox(height: 30.h),
+              DataItem(
+                label: 'رقم الهاتف',
+                value: viewModel?.profileInfo?.phoneNumber ?? '',
+              ),
+              SizedBox(height: 25.h),
+              DataItem(
+                label: 'اسم المستخدم',
+                value: viewModel?.profileInfo?.username ?? '',
+              ),
+              SizedBox(height: 25.h),
+              DataItem(
+                label: 'الاسم كامل',
+                value: viewModel?.profileInfo?.fullname ?? '',
+              ),
+              SizedBox(height: 25.h),
+              DataItem(
+                label: 'اللقب',
+                value: viewModel?.profileInfo?.familyName ?? '',
+              ),
+              SizedBox(height: 25.h),
+              DataItem(
+                label: 'البريد الإلكتروني',
+                value: viewModel?.profileInfo?.email ?? '',
+              ),
+              const BottomPadding(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RefreshView extends ViewModelWidget<ProfileViewModel> {
+  const _RefreshView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ProfileViewModel viewModel) {
+    return RefreshIndicator(
+      backgroundColor: kcolorPrimaryBlue,
+      color: Colors.white,
+      onRefresh: () => viewModel.initilizeView(),
+      child: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height - kToolbarHeight - 120,
+            alignment: Alignment.center,
+            child: Text(
+              'لايوجد بيانات, قم بسحب الصفحة لاسفل لتحديثها.',
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+              softWrap: true,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/ui/views/profile/profile_viewmodel.dart
+++ lib/ui/views/profile/profile_viewmodel.dart
@@ -1,3 +1,24 @@
+import 'package:huayati/app/locator.dart';
+import 'package:huayati/models/profile_info.dart';
+import 'package:huayati/services/auth_service.dart';
+import 'package:huayati/services/third_party/snackbar_service.dart';
 import 'package:stacked/stacked.dart';
 
-class ProfileViewModel extends BaseViewModel {}
+class ProfileViewModel extends BaseViewModel {
+  final _authService = locator<AuthService>();
+  final _snackbarService = locator<SnackbarService>();
+
+  ProfileInfo profileInfo;
+
+  Future<void> initilizeView() async {
+    try {
+      profileInfo = await runBusyFuture(
+        _authService.getProfileInfo(),
+        throwException: true,
+      );
+    } catch (e) {
+      print(e);
+      _snackbarService.showBottomErrorSnackbar(message: e.toString());
+    }
+  }
+}
