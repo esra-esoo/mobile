7a27313334b4427b6bf419e3b305c35713257c7b
Youssef Gheith <you.gheith@gmail.com>
2021-05-05 15:54:18 +0200

    add update button func

--- lib/ui/views/profile/profile_viewmodel.dart
+++ lib/ui/views/profile/profile_viewmodel.dart
@@ -1,12 +1,14 @@
 import 'package:huayati/app/locator.dart';
 import 'package:huayati/models/profile_info.dart';
 import 'package:huayati/services/auth_service.dart';
+import 'package:huayati/services/third_party/navigation_service.dart';
 import 'package:huayati/services/third_party/snackbar_service.dart';
 import 'package:stacked/stacked.dart';
 
 class ProfileViewModel extends BaseViewModel {
   final _authService = locator<AuthService>();
   final _snackbarService = locator<SnackbarService>();
+  final _navigationService = locator<NavigationService>();
 
   ProfileInfo profileInfo;
 
@@ -19,6 +21,16 @@ class ProfileViewModel extends BaseViewModel {
     } catch (e) {
       print(e);
       _snackbarService.showBottomErrorSnackbar(message: e.toString());
+    }
+  }
+
+  Future updateProfile() async {
+    //TODO  Navigation to update profile view
+    // var updatedProfile = await _navigationService.navigateTo('');
+    var updatedProfile = profileInfo;
+    if (updatedProfile != null) {
+      profileInfo = updatedProfile;
+      notifyListeners();
     }
   }
 }
