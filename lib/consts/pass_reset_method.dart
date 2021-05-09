e4a4d4e95d42fe30dcab50247014240dd1cabae1
Youssef Gheith <you.gheith@gmail.com>
2021-05-09 13:58:36 +0200

    models

---
+++ lib/consts/pass_reset_method.dart
@@ -0,0 +1,9 @@
+class ResetMethod {
+  static const String EMAIL = 'البريد الالكتروني';
+  static const String PHONE = 'رقم الهاتف';
+}
+
+class ResetMethodValue {
+  static const int EMAIL = 1;
+  static const int PHONE = 2;
+}
---
+++ lib/consts/pass_sent_by.dart
@@ -0,0 +1,9 @@
+class SentBy {
+  static const String EMAIL = 'email';
+  static const String SMS = 'sms';
+}
+
+class SentByValue {
+  static const int EMAIL = 1;
+  static const int SMS = 2;
+}
