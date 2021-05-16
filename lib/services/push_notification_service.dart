4dd5726c85bed350797ae02c783cd2123c47bea3
Youssef Gheith <you.gheith@gmail.com>
2021-05-16 02:30:51 +0200

    push notification service

---
+++ lib/services/push_notification_service.dart
@@ -0,0 +1,108 @@
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:get/get.dart';
+import 'package:huayati/app/app.locator.dart';
+import 'package:huayati/app/app_config.dart';
+import 'package:huayati/consts/storage_keys.dart';
+import 'package:huayati/models/company/refuse_state.dart';
+import 'package:huayati/models/individual/refuse_state.dart';
+import 'package:huayati/services/shared_service.dart';
+import 'package:huayati/services/third_party/snackbar_service.dart';
+
+import 'third_party/secure_storage_service.dart';
+
+class PushNotificationService {
+  final _fcm = FirebaseMessaging.instance;
+
+  final _snackbarService = locator<SnackBarsService>();
+  final _sharedService = locator<SharedService>();
+  final _secureStorageService = locator<SecureStorageService>();
+
+  Future<void> initialise() async {
+    try {
+      if (Platform.isIOS) {
+        await _fcm.requestPermission(
+          alert: true,
+          announcement: false,
+          badge: true,
+          carPlay: false,
+          criticalAlert: false,
+          provisional: false,
+          sound: true,
+        );
+      }
+      var alreadySubscribed = await _secureStorageService.readBoolean(
+        StorageKeys.SUBSCRIBED,
+      );
+      if (!alreadySubscribed) await _subscribeToDefaultTopic();
+
+      FirebaseMessaging.onMessage
+          .listen((RemoteMessage message) => _handleForgroundMessages(message));
+
+      FirebaseMessaging.onMessageOpenedApp.listen(
+          (RemoteMessage message) => _handleBackgroundMessages(message));
+
+      _handleTerminatedBackgroundMessages();
+    } catch (e) {
+      print('notification service initialise error => $e');
+    }
+  }
+
+  Future _subscribeToDefaultTopic() async {
+    final isDevEnviroment = AppConfig.of(Get.context!)!.isDevEnviroment;
+
+    isDevEnviroment
+        ? await _fcm.subscribeToTopic('dev_all_users')
+        : await _fcm.subscribeToTopic('all_users');
+
+    await _secureStorageService.addBoolean(StorageKeys.SUBSCRIBED, true);
+  }
+
+  Future unSubscribeFromDefaultTopic() async {
+    final isDevEnviroment = AppConfig.of(Get.context!)!.isDevEnviroment;
+
+    isDevEnviroment
+        ? await _fcm.unsubscribeFromTopic('dev_all_users')
+        : await _fcm.unsubscribeFromTopic('all_users');
+
+    await _secureStorageService.addBoolean(StorageKeys.SUBSCRIBED, false);
+  }
+
+  Future<void> _handleForgroundMessages(RemoteMessage message) async {
+    if (message.notification?.title != null &&
+        message.notification?.body != null) {
+      _snackbarService.showNotificationSnackbar(
+        title: message.notification!.title!,
+        message: message.notification!.body!,
+      );
+    }
+    _updateAccountRefuseStateFromMessage(message);
+  }
+
+  Future<void> _handleBackgroundMessages(RemoteMessage message) async {
+    _updateAccountRefuseStateFromMessage(message);
+  }
+
+  Future<void> _handleTerminatedBackgroundMessages() async {
+    RemoteMessage? initialMessage = await _fcm.getInitialMessage();
+    if (initialMessage != null) {
+      _updateAccountRefuseStateFromMessage(initialMessage);
+    }
+  }
+
+  void _updateAccountRefuseStateFromMessage(RemoteMessage message) {
+    if (message.data['refuse_state'] != null) {
+      String? accountType = message.data['account_type'];
+      if (accountType == 'company') {
+        CompanyRefuseState? companyRefuseState =
+            CompanyRefuseState.fromJson(message.data['refuse_state']);
+        _sharedService.updateCompanyState(companyRefuseState);
+      } else if (accountType == 'indivisual') {
+        IndivisualRefuseState? indivisualRefuseState =
+            IndivisualRefuseState.fromJson(message.data['refuse_state']);
+        _sharedService.updateIndivisualState(indivisualRefuseState);
+      }
+    }
+  }
+}
