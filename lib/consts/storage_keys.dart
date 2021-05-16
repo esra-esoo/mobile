62d09cb4b7eb0418d0a8de865624de474a5eee3b
Youssef Gheith <you.gheith@gmail.com>
2021-05-16 02:30:21 +0200

    add notfication topic sub

--- lib/consts/storage_keys.dart
+++ lib/consts/storage_keys.dart
@@ -2,4 +2,5 @@ class StorageKeys {
   static const String CREDENTIALS = 'credentials';
   static const String TOKEN = 'token';
   static const String USER = 'user';
+  static const String SUBSCRIBED = 'fcm_subscribed';
 }
